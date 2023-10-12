d0cf720c55e172433c821affbc43b9855e46d062
kDyeong <jaguar7374@naver.com>
2023-10-12 17:35:59 +0900

    병합2

--- backend/src/main/java/com/zosh/model/User.java
+++ backend/src/main/java/com/zosh/model/User.java
@@ -1,26 +1,16 @@
 package com.zosh.model;
 
-<<<<<<< HEAD
-=======
 import java.time.LocalDateTime;
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
 import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-<<<<<<< HEAD
-=======
-import com.fasterxml.jackson.annotation.JsonManagedReference;
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-<<<<<<< HEAD
-=======
 import jakarta.persistence.FetchType;
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -64,28 +54,14 @@ public class User {
     
     private String bio;
     
-<<<<<<< HEAD
-=======
     private String education;
     
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
     private boolean req_user;
     
     private boolean login_with_google;
     
     private boolean is_req_user=false;
     
-<<<<<<< HEAD
-//    @ManyToMany(mappedBy = "retwitUser",cascade = CascadeType.ALL)
-//    private List<Twit> retwits = new ArrayList<>();
-    
-    @JsonIgnore
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
-    private List<Twit> twit = new ArrayList<>();
-    
-    @JsonIgnore
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
-=======
     private LocalDateTime joinedAt; 
     
 //    @ManyToMany(mappedBy = "retwitUser",cascade = CascadeType.ALL)
@@ -97,7 +73,6 @@ public class User {
     
     @JsonIgnore
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
     private List<Like> likes  = new ArrayList<>();
     
     @Embedded
@@ -105,20 +80,11 @@ public class User {
     
     @JsonIgnore
     @ManyToMany(cascade = CascadeType.ALL)
-<<<<<<< HEAD
-    private List<User>followers=new ArrayList<>();
-    
-    @JsonIgnore
-    @ManyToMany(mappedBy = "followers")
-    private List<User>followings=new ArrayList<>();
-    
-=======
     private List<User> followers=new ArrayList<>();
     
     @JsonIgnore
     //@ManyToMany(mappedBy = "followers")
     @ManyToMany(cascade = CascadeType.ALL) // followers와 followings 테이블 분리를 통해 null칸 폐기
     private List<User> followings=new ArrayList<>();
->>>>>>> 6ae8e9f75dbac32ee0f1ba08023b9b4a9a6289f3
     
 }
