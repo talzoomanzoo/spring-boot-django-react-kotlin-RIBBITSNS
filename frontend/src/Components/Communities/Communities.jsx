558d8271c816570e25863847e4546b05e29e53ea
kDyeong <jaguar7374@naver.com>
2023-11-17 09:53:20 +0900

    ..

--- frontend/src/Components/Communities/Communities.jsx
+++ frontend/src/Components/Communities/Communities.jsx
@@ -3,10 +3,8 @@ import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { EffectCoverflow, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { getAllComs } from "../../Store/Community/Action";
 import ComBottom from "./ComBottom";
 import ComCard from "./ComCard";
@@ -29,7 +27,6 @@ const Communities = () => {
 
   console.log("comcheck", com);
 
-<<<<<<< HEAD
   return (
     <div id="coms" className="space-y-5">
       <section className={`z-50 flex items-center sticky top-0 bg-opacity-95`}>
@@ -39,29 +36,14 @@ const Communities = () => {
             onClick={handleBack}
           />
           <h1 className="py-5 text-xl font-bold opacity-90 ml-5">커뮤니티</h1>
-=======
-    return (
-        <div id="coms" className="space-y-5">
-            <section
-                className={`z-50 flex items-center sticky top-0 bg-opacity-95`}
-            >
-                <div className="z-50 flex items-center sticky top-0 space-x-5">
-                    <KeyboardBackspaceIcon
-                        className="cursor-pointer"
-                        onClick={handleBack}
-                    />
-                    <h1 className="py-5 text-xl font-bold opacity-90 ml-5">
-                        커뮤니티
-                    </h1>
-                </div>
-                <div
-                    className="absolute right-0 cursor-pointer" // 오른쪽 정렬, 클릭 커서
-                    onClick={handleOpenComModel} //커뮤니티 추가
-                >
-                    <GroupAddIcon /> 커뮤니티 추가
-
-                </div>
-            </section>
+        </div>
+        <div
+          className="absolute right-0 cursor-pointer" // 오른쪽 정렬, 클릭 커서
+          onClick={handleOpenComModel} //커뮤니티 추가
+        >
+          <GroupAddIcon /> 커뮤니티 추가
+        </div>
+      </section>
 
             <div>
                 <div
@@ -85,85 +67,6 @@ const Communities = () => {
                 </section>
                 </div>
             </div>
-
-            <section className="space-y-5" style={{ marginTop: 50 }}>
-                <div
-                    className="space-y-3"
-                    style={{ marginTop: 10, fontSize: "larger", }}>
-                    내 커뮤니티 리빗 모아보기
-                    <hr
-                        style={{
-                            marginTop: 10,
-                            background: 'grey',
-                            color: 'grey',
-                            borderColor: 'grey',
-                            height: '1px',
-                        }}
-                    />
-                </div>
-
-                <div>
-                    <ComBottom />
-                </div>
-            </section>
-
-            <section>
-                <ComModel
-                    open={openComModel}
-                    handleClose={handleCloseComModel}
-                />
-            </section>
->>>>>>> ce9284f945e2b32728d91ca7fa35f6463e248bd2
-        </div>
-        <div
-          className="absolute right-0 cursor-pointer" // 오른쪽 정렬, 클릭 커서
-          onClick={handleOpenComModel} //커뮤니티 추가
-        >
-          <GroupAddIcon /> 커뮤니티 추가
-        </div>
-      </section>
-
-      <div>
-        <div
-          className="space-y-3"
-          style={{ marginTop: 10, fontSize: "larger" }}
-        >
-          커뮤니티 찾아보기
-          <hr
-            style={{
-              marginTop: 10,
-              background: "grey",
-              color: "grey",
-              borderColor: "grey",
-              height: "1px",
-            }}
-          />
-        </div>
-
-        <div className="flex inline-block justify-content border-gray-700 h-[35vh] w-full rounded-md">
-          <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={2}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            modules={[EffectCoverflow, Pagination]}
-            className="swiper"
-          >
-            {com.coms.map((item) => (
-              <SwiperSlide>
-                <ComCard style={{ marginTop: 10 }} com={item} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
 
       <section className="space-y-5" style={{ marginTop: 50 }}>
         <div
